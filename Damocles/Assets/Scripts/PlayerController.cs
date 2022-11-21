c2331dbeb0f1685981bdc049a12f6eb8bb40a315
Cirilo <1adcirilo@seattleschools.org>
2022-11-21 09:44:49 -0800

    Movement is almost stolen

--- Damocles/Assets/Scripts/PlayerController.cs
+++ Damocles/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,15 @@ public class PlayerController : MonoBehaviour
     private BoxCollider2D boxCollider;
     private Vector2 velocity;
 
+    private bool grounded;
+
     private float speed = 10;
     float jumpHeight = 4;
 
     float walkAcceleration = 75;
     float airAcceleration = 30;
     float groundDeceleration = 70;
+
 
 
     // Start is called before the first frame update
@@ -25,11 +28,52 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.Translate(velocity * Time.deltaTime);
-
         float horiMoveInput = Input.GetAxisRaw("Horizontal");
-        velocity.x = Mathf.MoveTowards(velocity.x, speed * horiMoveInput, walkAcceleration * Time.deltaTime);
-        float vertMoveInput = Input.GetAxisRaw("Vertical");
-        velocity.y = Mathf.MoveTowards(velocity.y, speed * vertMoveInput, walkAcceleration * Time.deltaTime);
+
+        if (grounded)
+        {
+            velocity.y = 0;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                // Calculate the velocity required to achieve the target jump height.
+                velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+            }
+        }
+
+        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+
+
+        if (horiMoveInput != 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * horiMoveInput, walkAcceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
+        }
+
+        // Collider stuff (very cool, just don't look at it)
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+        grounded = false;
+
+        foreach (Collider2D hit in hits)
+        {
+            if (hit == boxCollider)
+                continue;
+
+            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+            if (colliderDistance.isOverlapped)
+            {
+                transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+
+                if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && velocity.y < 0)
+                {
+                    grounded = true;
+                }
+            }
+        }
 
     }
 }
