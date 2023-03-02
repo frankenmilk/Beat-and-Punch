5192fba9a05e607cc2e3add0d79bce22a421effd
Cirilo <1adcirilo@seattleschools.org>
2023-03-02 09:30:38 -0800

    Newest version :)

---
+++ Damocles/Assets/Scripts/CurrentGameInfo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentGameInfo : MonoBehaviour
+{
+    // This group is for what difficulty is on
+    public bool EasyOn;
+    public bool MediumOn;
+    public bool HardOn;
+    public bool DevOn;
+
+
+
+
+
+
+
+}
