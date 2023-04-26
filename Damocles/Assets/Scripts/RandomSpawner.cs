b3a58c5c87576cab37b5704974441b68a43595ac
Cirilo <1adcirilo@seattleschools.org>
2023-04-26 09:30:36 -0700

    asdasdasd

---
+++ Damocles/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for ()
+        int randEnemy = Random.Range(0, enemyPrefabs.Length);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
