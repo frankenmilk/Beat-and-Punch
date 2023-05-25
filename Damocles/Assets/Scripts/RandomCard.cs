f599d5c8f1ee452cd445aeda15fb966f77cef9c4
Cirilo <1adcirilo@seattleschools.org>
2023-05-25 09:45:12 -0700

    Changed up some of the code to make it work if it somehow chooses the same number twice

--- Damocles/Assets/Scripts/RandomCard.cs
+++ Damocles/Assets/Scripts/RandomCard.cs
@@ -7,7 +7,7 @@ public class RandomCard : MonoBehaviour
 {
 
     public Transform[] spawnPoints;
-    List<int> cardNumbers = new List<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+    public List<int> cardNumbers;   // = new List<int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
     public GameObject[] cardObjects;
 
 
@@ -19,10 +19,11 @@ public class RandomCard : MonoBehaviour
 
         for (int i = 0; i < spawnPoints.Length; i++)
         {
-            int randCard = Random.Range(1, cardNumbers.Count);
-
+            int randCard = Random.Range(0, cardNumbers.Count);
+            
             cardObjects[randCard].transform.position = new Vector2(spawnPoints[spawnPointNum].position.x, spawnPoints[spawnPointNum].position.y);
             cardNumbers.RemoveAt(randCard);
+            Debug.Log(randCard + 1);
 
             spawnPointNum++;
         }
