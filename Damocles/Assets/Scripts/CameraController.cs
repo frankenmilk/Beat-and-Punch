18ca066c3b763fa0fadd2a94fcbc2c3061d7723f
1Naprestegard <naprestegard@gmail.com>
2023-06-16 11:34:27 -0700

    cameraoffset

--- Damocles/Assets/Scripts/CameraController.cs
+++ Damocles/Assets/Scripts/CameraController.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
     {
         transform.position = target.position + offset; 
+        /*if (SceneManager.GetActiveScene().buildIndex == 5 && 
+        38.5 < player.transform.position.x && 37.1 < player.transform.position.y)
+        {
+            offset.y = 3.5;
+        }*/
     }
 }
